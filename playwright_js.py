5c25da7f7600b8d782df1bdccacc4ee05c0a279b
ElenaRazina <elenarazina87@yandex.ru>
2026-03-03 15:16:22 +0300

    Playwright углубление_3

---
+++ playwright_js.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright:
+    """Запускаем браузер"""
+    browser=playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    """Ждем, когда загрузится страница"""
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login", wait_until="networkidle")
+
+    """Запускаем скрипты - первый способ"""
+    #text='New Text'
+    #page.evaluate(
+    #    """
+    #    const title = document.getElementById('authentication-ui-course-title-text')
+    #    title.textContent='{text}'
+    #   """
+    #)
+    """Запускаем скрипты - второй способ через безымянную функцию"""
+    new_text='New Text'
+    page.evaluate(
+        """
+        (text) => {
+            const title = document.getElementById('authentication-ui-course-title-text')
+            title.textContent = text
+        }
+        """,
+        new_text
+    )
+    page.wait_for_timeout(3000)
