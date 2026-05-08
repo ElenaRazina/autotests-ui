e567432fd9ba8df9e9c88c328b78dccd4222aead
ElenaRazina <elenarazina87@yandex.ru>
2026-05-08 15:49:02 +0300

    Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/button.py
+++ elements/button.py
@@ -2,6 +2,7 @@ from playwright.sync_api import expect
 from elements.base_element import BaseElement
 import allure
 from tools.logger import get_logger
+from ui_coverage_tool import ActionType
 
 logger=get_logger("BUTTON")
 
@@ -10,6 +11,11 @@ class Button(BaseElement):
     def type_of(self) ->str:
         return 'button'
 
+    def get_raw_locator(self,
+                        nth: int = 0,
+                        **kwargs) -> str:
+        return f"{super().get_raw_locator(nth, **kwargs)}"
+
     def check_enabled(self, nth: int = 0, **kwargs):
         step=f'Checking that {self.type_of} "{self.name}" is enabled'
         with allure.step(step):
@@ -17,9 +23,17 @@ class Button(BaseElement):
             logger.info(step)
             expect(locator).to_be_enabled()
 
+        self.track_coverage(
+            ActionType.ENABLED,
+            nth, **kwargs)
+
     def check_disabled(self, nth: int = 0, **kwargs):
         step=f'Checking that {self.type_of} "{self.name}" is disabled'
         with allure.step(step):
             locator = self.get_locator(nth,**kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
+
+        self.track_coverage(
+            ActionType.DISABLED,
+            nth, **kwargs)
